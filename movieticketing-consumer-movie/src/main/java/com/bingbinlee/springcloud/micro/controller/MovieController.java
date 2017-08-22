7262181da8e96e8e950392bcb29e5a6776f0e383
libingbin <libingbin2015@aliyun.com>
2017-08-22 08:28:12 +0800

    编写一个服务消费者(电影微服务)

---
+++ movieticketing-consumer-movie/src/main/java/com/bingbinlee/springcloud/micro/controller/MovieController.java
@@ -0,0 +1,20 @@
+package com.bingbinlee.springcloud.micro.controller;
+
+import com.bingbinlee.springcloud.micro.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+public class MovieController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+    }
+}
